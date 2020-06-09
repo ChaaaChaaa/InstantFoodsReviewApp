de555c3baa7a4e8035bb1e1400d6bd3dca5117cc
PeonyF <1621139@gmail.com>
2020-06-09 16:49:20 +0900

    [ADD] cardview in mainfragment and show next fragment fumction

---
+++ app/src/main/java/com/myapp/instantfoodsreviewapp/fragment/PizzaFragment.java
@@ -0,0 +1,26 @@
+package com.myapp.instantfoodsreviewapp.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.myapp.instantfoodsreviewapp.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class PizzaFragment extends Fragment {
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_pizza, container, false);
+    }
+}
