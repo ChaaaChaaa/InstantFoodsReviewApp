f02541c390276d420d0f5232e37a784c9af2c5f0
PeonyF <1621139@gmail.com>
2020-06-20 02:56:22 +0900

    [ADD] show my email,nickname in mypage

--- app/src/main/java/com/myapp/instantfoodsreviewapp/fragment/MyPageFragment.java
+++ app/src/main/java/com/myapp/instantfoodsreviewapp/fragment/MyPageFragment.java
@@ -11,8 +11,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.myapp.instantfoodsreviewapp.BuildConfig;
 import com.myapp.instantfoodsreviewapp.R;
+import com.myapp.instantfoodsreviewapp.model.UserAccountData;
+import com.myapp.instantfoodsreviewapp.model.entity.ApiResultDto;
+import com.myapp.instantfoodsreviewapp.preference.UserPreference;
+import com.myapp.instantfoodsreviewapp.restapi.RetrofitClient;
+import com.myapp.instantfoodsreviewapp.restapi.RetrofitInterface;
+import com.myapp.instantfoodsreviewapp.utils.Config;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MyPageFragment extends Fragment implements Button.OnClickListener {
     private TextView currentVersion;
@@ -20,6 +32,7 @@ public class MyPageFragment extends Fragment implements Button.OnClickListener {
     private TextView getEmail;
     private TextView btnChangePassword;
     private TextView btnSecession;
+    private UserPreference userPreference;
 
     @Nullable
     @Override
@@ -27,6 +40,8 @@ public class MyPageFragment extends Fragment implements Button.OnClickListener {
         View rootView = inflater.inflate(R.layout.fragment_my_page, container, false);
         init(rootView);
         initButton(rootView);
+        initPreference();
+        getUserInfo();
         return rootView;
     }
 
@@ -46,7 +61,35 @@ public class MyPageFragment extends Fragment implements Button.OnClickListener {
         btnSecession.setOnClickListener(this);
     }
 
+    private void initPreference(){
+        userPreference = new UserPreference();
+        userPreference.setContext(getContext());
+    }
+
     private void getUserInfo() {
+        String getToken = userPreference.getString(Config.KEY_TOKEN);
+        RetrofitInterface retrofitInterface = RetrofitClient.buildHTTPClient();
+        Call<ApiResultDto> call = retrofitInterface.account(getToken);
+        call.enqueue(new Callback<ApiResultDto>() {
+            @Override
+            public void onResponse(Call<ApiResultDto> call, Response<ApiResultDto> response) {
+                if(response.isSuccessful()){
+                    ApiResultDto dto = response.body();
+                    JsonObject resultData = dto.getResultData();
+                    if(resultData != null){
+                        UserAccountData userAccountData = new Gson().fromJson(resultData,UserAccountData.class);
+                        getEmail.setText(userAccountData.getEmail());
+                        currentNickName.setText(userAccountData.getNickname());
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ApiResultDto> call, Throwable t) {
+
+            }
+        });
+
 
     }
 
@@ -71,4 +114,6 @@ public class MyPageFragment extends Fragment implements Button.OnClickListener {
         SecessionDialog secessionDialog = new SecessionDialog();
         secessionDialog.show(getFragmentManager(), "secession dialog");
     }
+
+
 }
