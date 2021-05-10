95b55c0cebf2cc6217fb7f1ae7b67e98130399f5
MJ Choi <1621139@gmail.com>
2021-05-11 00:07:18 +0900

    [UPDATE] use data binding and change switch for if

--- app/src/main/java/com/myapp/instantfoodsreviewapp/fragment/HomeFragment.java
+++ app/src/main/java/com/myapp/instantfoodsreviewapp/fragment/HomeFragment.java
@@ -3,6 +3,7 @@ package com.myapp.instantfoodsreviewapp.fragment;
 import android.os.Bundle;
 
 import androidx.cardview.widget.CardView;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -12,6 +13,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.myapp.instantfoodsreviewapp.R;
+import com.myapp.instantfoodsreviewapp.databinding.FragmentHomeBinding;
 import com.myapp.instantfoodsreviewapp.fragment.product.ProductListDdokbokkiFragment;
 import com.myapp.instantfoodsreviewapp.fragment.product.ProductListDumplingFragment;
 import com.myapp.instantfoodsreviewapp.fragment.product.ProductListFriedRiceFragment;
@@ -19,32 +21,32 @@ import com.myapp.instantfoodsreviewapp.fragment.product.ProductListNoodleFragmen
 import com.myapp.instantfoodsreviewapp.fragment.product.ProductListPizzaFragment;
 import com.myapp.instantfoodsreviewapp.fragment.product.ProductListStewFragment;
 
+import org.jetbrains.annotations.NotNull;
 
-public class HomeFragment extends Fragment implements CardView.OnClickListener {
-    private ImageView image_coldNoodles;
-    private ImageView image_ddokbokki;
-    private ImageView image_dumpling;
-    private ImageView image_friedRice;
-    private ImageView image_pizza;
-    private ImageView image_stew;
+
+public class HomeFragment extends Fragment implements View.OnClickListener {
+    private FragmentHomeBinding fragmentHomeBinding;
+    private View.OnClickListener onClickListener;
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NotNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.fragment_home, container, false);
-        init(rootView);
+        fragmentHomeBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_home, container, false);
+        View rootView = fragmentHomeBinding.getRoot();
+        rootView.setOnClickListener(this);
+        init();
         onClick(rootView);
         return rootView;
     }
 
-    private void init(View view) {
-        image_coldNoodles = view.findViewById(R.id.image_coldNoodle);
-        image_ddokbokki = view.findViewById(R.id.image_ddokbokki);
-        image_dumpling = view.findViewById(R.id.image_dumpling);
-        image_friedRice = view.findViewById(R.id.image_friedRice);
-        image_pizza = view.findViewById(R.id.image_pizza);
-        image_stew = view.findViewById(R.id.image_stew);
+    private void init() {
+        ImageView image_coldNoodles = fragmentHomeBinding.imageColdNoodle;
+        ImageView image_ddokbokki = fragmentHomeBinding.imageDdokbokki;
+        ImageView image_dumpling = fragmentHomeBinding.imageDumpling;
+        ImageView image_friedRice = fragmentHomeBinding.imageFriedRice;
+        ImageView image_pizza = fragmentHomeBinding.imagePizza;
+        ImageView image_stew = fragmentHomeBinding.imageStew;
 
         image_coldNoodles.setOnClickListener(this);
         image_ddokbokki.setOnClickListener(this);
@@ -52,56 +54,50 @@ public class HomeFragment extends Fragment implements CardView.OnClickListener {
         image_friedRice.setOnClickListener(this);
         image_pizza.setOnClickListener(this);
         image_stew.setOnClickListener(this);
-
     }
 
     @Override
     public void onClick(View view) {
-
-        switch (view.getId()) {
-            case R.id.image_coldNoodle:
-                FragmentTransaction fragmentTransactionNoodle = getFragmentManager().beginTransaction();
-                fragmentTransactionNoodle.replace(R.id.fragment_container, new ProductListNoodleFragment());
-                fragmentTransactionNoodle.addToBackStack(null);
-                fragmentTransactionNoodle.commit();
-                break;
-
-            case R.id.image_ddokbokki:
-                FragmentTransaction fragmentTransactionddok = getFragmentManager().beginTransaction();
-                fragmentTransactionddok.replace(R.id.fragment_container, new ProductListDdokbokkiFragment());
-                fragmentTransactionddok.addToBackStack(null);
-                fragmentTransactionddok.commit();
-                break;
-
-            case R.id.image_dumpling:
-                FragmentTransaction fragmentTransactionDumpling = getFragmentManager().beginTransaction();
-                fragmentTransactionDumpling.replace(R.id.fragment_container, new ProductListDumplingFragment());
-                fragmentTransactionDumpling.addToBackStack(null);
-                fragmentTransactionDumpling.commit();
-                break;
-
-            case R.id.image_friedRice:
-                FragmentTransaction fragmentTransactionFriedRice = getFragmentManager().beginTransaction();
-                fragmentTransactionFriedRice.replace(R.id.fragment_container, new ProductListFriedRiceFragment());
-                fragmentTransactionFriedRice.addToBackStack(null);
-                fragmentTransactionFriedRice.commit();
-                break;
-
-            case R.id.image_pizza:
-                FragmentTransaction fragmentTransactionPizza = getFragmentManager().beginTransaction();
-                fragmentTransactionPizza.replace(R.id.fragment_container, new ProductListPizzaFragment());
-                fragmentTransactionPizza.addToBackStack(null);
-                fragmentTransactionPizza.commit();
-                break;
-
-            case R.id.image_stew:
-                FragmentTransaction fragmentTransactionStew = getFragmentManager().beginTransaction();
-                fragmentTransactionStew.replace(R.id.fragment_container, new ProductListStewFragment());
-                fragmentTransactionStew.addToBackStack(null);
-                fragmentTransactionStew.commit();
-                break;
+        if(view == fragmentHomeBinding.imageColdNoodle){
+            FragmentTransaction fragmentTransactionNoodle = getFragmentManager().beginTransaction();
+            fragmentTransactionNoodle.replace(R.id.fragment_container, new ProductListNoodleFragment());
+            fragmentTransactionNoodle.addToBackStack(null);
+            fragmentTransactionNoodle.commit();
         }
 
-    }
+        else if(view == fragmentHomeBinding.imageDdokbokki) {
+            FragmentTransaction fragmentTransactionddok = getFragmentManager().beginTransaction();
+            fragmentTransactionddok.replace(R.id.fragment_container, new ProductListDdokbokkiFragment());
+            fragmentTransactionddok.addToBackStack(null);
+            fragmentTransactionddok.commit();
+        }
 
+        else if(view == fragmentHomeBinding.imageDumpling) {
+            FragmentTransaction fragmentTransactionDumpling = getFragmentManager().beginTransaction();
+            fragmentTransactionDumpling.replace(R.id.fragment_container, new ProductListDumplingFragment());
+            fragmentTransactionDumpling.addToBackStack(null);
+            fragmentTransactionDumpling.commit();
+        }
+
+        else if(view == fragmentHomeBinding.imageFriedRice) {
+            FragmentTransaction fragmentTransactionFriedRice = getFragmentManager().beginTransaction();
+            fragmentTransactionFriedRice.replace(R.id.fragment_container, new ProductListFriedRiceFragment());
+            fragmentTransactionFriedRice.addToBackStack(null);
+            fragmentTransactionFriedRice.commit();
+        }
+
+        else if(view == fragmentHomeBinding.imagePizza) {
+            FragmentTransaction fragmentTransactionPizza = getFragmentManager().beginTransaction();
+            fragmentTransactionPizza.replace(R.id.fragment_container, new ProductListPizzaFragment());
+            fragmentTransactionPizza.addToBackStack(null);
+            fragmentTransactionPizza.commit();
+        }
+
+        else if(view == fragmentHomeBinding.imageStew) {
+            FragmentTransaction fragmentTransactionStew = getFragmentManager().beginTransaction();
+            fragmentTransactionStew.replace(R.id.fragment_container, new ProductListStewFragment());
+            fragmentTransactionStew.addToBackStack(null);
+            fragmentTransactionStew.commit();
+        }
+    }
 }
