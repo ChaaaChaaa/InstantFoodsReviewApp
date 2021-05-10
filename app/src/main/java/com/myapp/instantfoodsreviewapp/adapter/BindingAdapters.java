180bee1145d7695ed88423bc8aa84436bb941e7d
MJ Choi <1621139@gmail.com>
2021-05-11 00:03:26 +0900

    [DELETE] try to make binding adapters

---
+++ app/src/main/java/com/myapp/instantfoodsreviewapp/adapter/BindingAdapters.java
@@ -0,0 +1,28 @@
+package com.myapp.instantfoodsreviewapp.adapter;
+
+import android.widget.ImageView;
+
+import androidx.databinding.BindingAdapter;
+
+import com.bumptech.glide.Glide;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class BindingAdapters {
+//    @BindingAdapter("onNavigationItemSelected")
+//    public static void setOnNavigationItemSelected(
+//            NavigationView view, NavigationView.OnNavigationItemSelectedListener listener) {
+//        view.OnNavigationItemSelectedListener(listener);
+//    }
+//
+//    @BindingAdapter("selectedItemPosition")
+//    public static void setSelectedItemPosition(
+//            NavigationView view, int position) {
+//        view.onNavigationItemSelected(position);
+//    }
+
+//    @BindingAdapter({"bind:imageUrl"})
+//    public static void loadImage(ImageView view, String url) {
+//        Glide.with(view.getContext()).load(url).into(view);
+//    }
+}
