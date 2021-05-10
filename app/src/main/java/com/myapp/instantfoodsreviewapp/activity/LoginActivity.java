eea5af86e551a90090056db25503da8dabcf11cb
MJ Choi <1621139@gmail.com>
2021-05-10 23:57:59 +0900

    [UPDATE] change switch to if in Login Activity

--- app/src/main/java/com/myapp/instantfoodsreviewapp/activity/LoginActivity.java
+++ app/src/main/java/com/myapp/instantfoodsreviewapp/activity/LoginActivity.java
@@ -7,53 +7,41 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.view.GravityCompat;
 import androidx.databinding.DataBindingUtil;
 
 import com.myapp.instantfoodsreviewapp.R;
 import com.myapp.instantfoodsreviewapp.databinding.ActivityLoginBinding;
-import com.myapp.instantfoodsreviewapp.restapi.RetrofitClient;
-import com.myapp.instantfoodsreviewapp.restapi.RetrofitInterface;
-
-import retrofit2.Retrofit;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
     private Button loginButton;
-    private TextView loginTextView;
     private ActivityLoginBinding loginBinding;
-    private EmailLoginActivity emailLoginActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         loginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login);
         init();
-
-
     }
 
     public void init() {
         loginButton = loginBinding.loginButton;
         loginButton.setOnClickListener(this);
-        loginTextView = loginBinding.tvRegisterEmail;
+        TextView loginTextView = loginBinding.tvRegisterEmail;
         loginTextView.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        Intent intent = null;
-        switch (view.getId()) {
-            case R.id.login_button:
-                intent = new Intent(this, EmailLoginActivity.class);
+        Intent intent;
+        int buttonId = loginButton.getId();
 
-                break;
-            case R.id.tv_register_email:
-                intent = new Intent(this, RegisterActivity.class);
-                break;
+        if (view.getId() == buttonId) {
+            intent = new Intent(this, EmailLoginActivity.class);
+        } else {
+            intent = new Intent(this, RegisterActivity.class);
         }
+
         startActivity(intent);
         finish();
     }
-
-
 }
